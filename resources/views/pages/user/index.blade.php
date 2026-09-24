1fe8d8d98664da94abf728cedf1801f421e5aee0
I Wayan Arya Wardana <aryamadridfc@gmail.com>
2026-09-24 21:23:10 +0800

    imporve login page

--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -104,6 +104,10 @@
                         name: 'role'
                     },
                     {
+                        data: 'status',
+                        name: 'status'
+                    },
+                    {
                         data: 'created_at',
                         name: 'created_at'
                     },
